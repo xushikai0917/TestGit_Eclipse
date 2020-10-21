519123d28d303585e192eab7ada932fa2f494503
eclipse_user <1985363387@qq.com>
2020-10-21 12:23:12 +0800

    First Commit in Eclipse

---
+++ src/main/java/com/xsk/git/Apple.java
@@ -0,0 +1,9 @@
+package com.xsk.git;
+
+public class Apple {
+	
+	public void hello() {
+		System.out.println("Hello");
+	}
+	
+}
---
+++ src/test/java/com/xsk/test/GoodTest.java
@@ -0,0 +1,9 @@
+package com.xsk.test;
+
+public class GoodTest {
+	
+	public static void main(String[] args) {
+		System.out.println("hello good test");
+	}
+	
+}
